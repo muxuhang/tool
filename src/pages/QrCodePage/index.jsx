bb5df4dbacfdf3077fc2117db9d9f1400ff381e2
muxuhang <muxuhang@gmail.com>
2024-11-26 10:25:33 +0800

    生成二维码

--- src/pages/QrCodePage/index.jsx
+++ src/pages/QrCodePage/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Input, Row, Space, Switch, Upload } from 'antd'
+import { Button, Col, Input, Row, Switch, Upload } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
 import TextArea from 'antd/lib/input/TextArea'
 import React, { useEffect, useState } from 'react'
@@ -9,7 +9,6 @@ function QrCodePage() {
   const [size, setSize] = useState(160)
   const [source, setSource] = useState('')
   const [imageSetting, setImageSetting] = useState(null)
-  const [imgUrl, setImgUrl] = useState('')
   useEffect(() => {
     setImageSetting({
       src: source,
@@ -28,67 +27,66 @@ function QrCodePage() {
     dom.download = new Date().getTime() + '.png'
     dom.click()
   }
-  function base64ToBlob(base64Data, contentType) {
-    contentType = contentType || ''
-    const sliceSize = 1024
-    const byteCharacters = atob(base64Data.split(',')[1])
-    const byteArrays = []
+  // function base64ToBlob(base64Data, contentType) {
+  //   contentType = contentType || ''
+  //   const sliceSize = 1024
+  //   const byteCharacters = atob(base64Data.split(',')[1])
+  //   const byteArrays = []
 
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize)
+  //   for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+  //     const slice = byteCharacters.slice(offset, offset + sliceSize)
 
-      const byteNumbers = new Array(slice.length)
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i)
-      }
+  //     const byteNumbers = new Array(slice.length)
+  //     for (let i = 0; i < slice.length; i++) {
+  //       byteNumbers[i] = slice.charCodeAt(i)
+  //     }
 
-      const byteArray = new Uint8Array(byteNumbers)
-      byteArrays.push(byteArray)
-    }
+  //     const byteArray = new Uint8Array(byteNumbers)
+  //     byteArrays.push(byteArray)
+  //   }
 
-    const blob = new Blob(byteArrays, { type: contentType })
-    return blob
-  }
-  function downloadBlob(blob, filename) {
-    const a = document.createElement('a')
-    const url = URL.createObjectURL(blob)
-    a.href = url
-    a.download = filename
-    a.style.display = 'none'
-    document.body.appendChild(a)
-    a.click()
-    URL.revokeObjectURL(url)
-    document.body.removeChild(a)
-  }
-  const BlobDownload = () => {
-    const canvas = document.getElementById('qrcode')
-    const image = new Image()
-    image.src = canvas.toDataURL('image/png')
-    const contentType = image.src.split(':')[1].split(';')[0]
-    const blob = base64ToBlob(image.src, contentType)
-    downloadBlob(blob, 'downloaded_image.png')
-  }
-  const showImage = () => {
-    const canvas = document.getElementById('qrcode')
-    const image = new Image()
-    image.src = canvas.toDataURL('image/png')
-    setImgUrl(image.src)
-  }
-  const clearImage = () => {
-    setImgUrl('')
-  }
+  //   const blob = new Blob(byteArrays, { type: contentType })
+  //   return blob
+  // }
+  // function downloadBlob(blob, filename) {
+  //   const a = document.createElement('a')
+  //   const url = URL.createObjectURL(blob)
+  //   a.href = url
+  //   a.download = filename
+  //   a.style.display = 'none'
+  //   document.body.appendChild(a)
+  //   a.click()
+  //   URL.revokeObjectURL(url)
+  //   document.body.removeChild(a)
+  // }
+  // const BlobDownload = () => {
+  //   const canvas = document.getElementById('qrcode')
+  //   const image = new Image()
+  //   image.src = canvas.toDataURL('image/png')
+  //   const contentType = image.src.split(':')[1].split(';')[0]
+  //   const blob = base64ToBlob(image.src, contentType)
+  //   downloadBlob(blob, 'downloaded_image.png')
+  // }
+  // const showImage = () => {
+  //   const canvas = document.getElementById('qrcode')
+  //   const image = new Image()
+  //   image.src = canvas.toDataURL('image/png')
+  //   setImgUrl(image.src)
+  // }
+  // const clearImage = () => {
+  //   setImgUrl('')
+  // }
   return (
-    <div className='container qr-code'>
-      <Row style={{ margin: 10 }}>
-        <Col span={24}>
+    <div className='m-3 container mx-auto'>
+      <Row gutter={16}>
+        <Col flex={1}>
           <TextArea
-            rows={6}
+            rows={10}
             value={text}
             onChange={(e) => setText(e.target.value)}
-            className='left_input'
             placeholder='输入文本内容'></TextArea>
         </Col>
-        <Col style={{ padding: 10 }}>
+        <Col>
           <QRCodeCanvas
             id='qrcode'
             value={text}
@@ -96,80 +94,65 @@ function QrCodePage() {
             style={{ height: '12rem', width: '12rem' }}
             fgColor='#000000'
             imageSettings={useicon ? imageSetting : null}></QRCodeCanvas>
-        </Col>
-        <Col style={{ padding: '10px', flex: 1 }}>
-          <div className='right_option'>
-            <label className='option_label'>使用Logo</label>
-            <Switch
-              className='option_val'
-              checked={useicon}
-              onChange={(e) => setuseIcon(e)}></Switch>
-          </div>
-          {useicon ? (
-            <div className='right_option'>
-              <label className='option_label'>上传Logo</label>
-              <Upload
-                name='logo'
-                customRequest={() => null}
-                showUploadList={false}
-                onChange={(info) => {
-                  const reader = new FileReader()
-                  reader.addEventListener('load', () =>
-                    setSource(reader.result)
-                  )
-                  reader.readAsDataURL(info.file.originFileObj)
-                }}>
-                <img
-                  src={source}
-                  alt=''
-                  style={{ height: 100, width: 100, display: 'none' }}></img>
-                <Button type='primary'>
-                  <UploadOutlined />
-                </Button>
-              </Upload>
+          <div className='flex items-center my-2'>
+            <div className='mr-2'>
+              <label className='label mr-2'>使用Logo</label>
+              <Switch
+                className='option_val'
+                checked={useicon}
+                onChange={(e) => setuseIcon(e)}></Switch>
             </div>
-          ) : null}
-          <div className='right_option'>
-            <label className='option_label'>图片大小</label>
-            <Input
-              value={size}
-              className='option_val'
-              type='number'
-              onChange={(e) => {
-                let newSize = parseFloat(e.target.value)
-                if (newSize < 100) setSize(100)
-                else if (newSize > 3000) setSize(3000)
-                else setSize(newSize)
-              }}></Input>
+            {useicon && (
+              <>
+                <label className='label mr-2'>上传Logo</label>
+                <Upload
+                  name='logo'
+                  customRequest={() => null}
+                  showUploadList={false}
+                  onChange={(info) => {
+                    const reader = new FileReader()
+                    reader.addEventListener('load', () =>
+                      setSource(reader.result)
+                    )
+                    reader.readAsDataURL(info.file.originFileObj)
+                  }}>
+                  <img
+                    src={source}
+                    alt=''
+                    style={{ height: 100, width: 100, display: 'none' }}></img>
+                  <Button type='primary'>
+                    <UploadOutlined />
+                  </Button>
+                </Upload>
+              </>
+            )}
           </div>
-          <div className='right_option'>
-            <Space>
-              <Button
-                type='primary'
-                onClick={CanvasToImage}>
-                下载二维码
-              </Button>
-              <Button
-                type='primary'
-                onClick={BlobDownload}>
-                Blob下载
-              </Button>
-              <Button
-                type='primary'
-                onClick={showImage}>
-                生成图片
-              </Button>
-              <Button onClick={clearImage}>清除</Button>
-            </Space>
-            <div style={{ flex: 1 }}></div>
+          <div className=''>
+            <label className='label'>图片大小</label>
+            <div className='flex mt-2'>
+              <Input
+                value={size}
+                type='number'
+                className='mr-2'
+                onChange={(e) => {
+                  let newSize = parseFloat(e.target.value)
+                  if (newSize < 100) setSize(100)
+                  else if (newSize > 3000) setSize(3000)
+                  else setSize(newSize)
+                }}></Input>
+              <Button type='primary' onClick={CanvasToImage}>下载二维码</Button>
+            </div>
+          </div>
+          <div className='py-2 flex gap-2'>
+            {/* <Button type='primary' onClick={BlobDownload}>Blob下载</Button> */}
+            {/* <Button type='primary' onClick={showImage}>生成图片</Button>
+            <Button onClick={clearImage}>清除</Button> */}
           </div>
         </Col>
       </Row>
-      <img
-        src={imgUrl}
-        style={{ width: '100%', zIndex: 100, maxWidth: 400 }}
-        alt=''
-      />
+      {/* <div className='flex items-center justify-center p-2'>
+        <img src={imgUrl} alt='' />
+      </div> */}
     </div>
   )
 }
